8c0cf015d1cbb928ea3d051adf1edb5b1750cd25
Karthik <karthikwebdev@outlook.com>
2024-02-21 19:45:45 +0530

    fix layout issue

--- src/components/shared/Home.tsx
+++ src/components/shared/Home.tsx
@@ -34,7 +34,7 @@ const Home: React.FC = () => {
   }, [url]);
   return (
    
-    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="h-screen   w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
     <Spotlight
       className="-top-40 left-0 md:left-60 md:-top-20"
       fill="white"
