0ece42fd9b3b7a081b02b464d1043a4dc681eb9e
Karthik <karthikwebdev@outlook.com>
2024-02-21 13:44:36 +0530

    create a project using next ui and tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+const { nextui } = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
