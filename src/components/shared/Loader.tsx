4606474fe9cf8e19cfba9e21d2912dcbac043e41
Karthik <karthikwebdev@outlook.com>
2024-02-21 19:45:12 +0530

    add the loading skeleton

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,23 @@
+import { Skeleton } from "@nextui-org/react"
+
+
+const Loader = () => {
+  return (
+    <div className=" text-gray-400 relative">
+    <div className="absolute top-0 z-[-2] h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+    
+    <div className="max-w-7xl   mx-auto relative  pt-24 ">
+        <Skeleton className="max-w-3xl w-full h-5 bg-neutral-950/50 mx-auto rounded-lg" />
+       <div className="space-y-4 mt-10 font-normal text-base  max-w-3xl text-center mx-auto">
+       <Skeleton className="max-w-3xl w-full h-2 bg-neutral-950/50 mx-auto rounded-full" />
+       <Skeleton className="max-w-2xl w-full h-2 bg-neutral-950/50 mx-auto rounded-full" />
+       <Skeleton className="max-w-3xl w-full h-2 bg-neutral-950/50 mx-auto rounded-full" />
+       <Skeleton className="max-w-xl w-full h-2 bg-neutral-950/50 mx-auto rounded-lg" />
+       </div>
+    </div>
+    <Skeleton className="max-w-4xl mx-auto w-full h-40 mt-20 bg-neutral-950/50 mx-auto rounded-lg" />
+       </div>
+  )
+}
+
+export default Loader
--- src/components/shared/Results.tsx
+++ src/components/shared/Results.tsx
@@ -2,8 +2,12 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import {Tabs, Tab, Card, CardBody} from "@nextui-org/react";
+import {Tabs, Tab} from "@nextui-org/react";
 import Detection from "./Detection";
+import Stats from './Stats';
+import Details from './Details';
+import Loader from './Loader';
+
 
 const Results: React.FC = () => {
   const { urlId } = useParams<{ urlId: string }>();
@@ -42,34 +46,49 @@ const Results: React.FC = () => {
     };
   }, [urlId]);
   if (loading) {
-    return <div>Loading...</div>;
+    return <Loader />
   }
 
   if (error) {
     return <div>Error: {error}</div>;
   }
 
+  const { attributes:{times_submitted,url,tld,title,first_submission_date,reputation,total_votes} } = urlInfo;
+
   return (
-    <div className="container mx-auto py-8">
-      <h2 className="text-lg font-semibold mb-2">URL Information:</h2>
-      
+    <div className=" text-gray-400 relative">
+     
+ <div className="absolute top-0 z-[-2] h-full w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+ 
+ <div className="  max-w-7xl   mx-auto relative  pt-20 ">
+      <h1 className="text-4xl pb-3 md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+      URL Information
+      </h1>
+      <p className="mt-4 font-normal text-base text-neutral-300 max-w-3xl text-center mx-auto">
+      The URL <strong>{url}</strong> titled <strong>{title}</strong> has been submitted <strong>{times_submitted}</strong> times. It belongs to the domain <strong>{tld}</strong> and was first submitted on <strong>{first_submission_date}</strong>. It has a reputation score of <strong>{reputation}</strong> based on <strong>{total_votes.harmless} harmless votes</strong>.
+  
+  The crowdsourced context for this URL includes insights on severity, source, and timestamp. The last analysis results are provided below. This offers a comprehensive overview of the URL data, aiding in understanding its significance and potential risks.
+      </p>
+    
+ 
+    </div>
+
+ 
+    
+      {urlInfo && <Stats data={urlInfo}  /> }
       <div className="flex w-full flex-col">
-      <Tabs radius="full" variant="bordered" aria-label="Options">
+      <Tabs className='px-2 max-w-5xl mx-auto' radius="full" variant="bordered" aria-label="Options">
         <Tab key="detection" title="
         Detection">
         {urlInfo && <Detection data={urlInfo}  /> }
         </Tab>
         <Tab key="summary" title="Summary">
-          <Card>
-            <CardBody>
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            </CardBody>
-          </Card>  
+        {urlInfo && <Details data={urlInfo}  /> }
         </Tab>
       
       </Tabs>
     </div>  
-      <pre>{JSON.stringify(urlInfo, null, 2)}</pre>
+    
     </div>
   );
 };
