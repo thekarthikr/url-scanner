92b08b0e47bf2289d9e319e9a68300112e14caa4
Karthik <karthikwebdev@outlook.com>
2024-02-22 15:42:45 +0530

    update nav

--- src/components/shared/Nav.tsx
+++ src/components/shared/Nav.tsx
@@ -4,8 +4,8 @@ import logo from '../../../public/favicon.png'
 
 const Nav = () => {
   return (
-    <header className="fixed z-50 bg-neutral-900/30 backdrop-blur-md w-full top-0 ">
-        <nav className="flex justify-between items-center max-w-6xl mx-auto py-3 border-b border-neutral-700/50 px-3 text-gray-300">
+    <header className="fixed z-50 bg-neutral-900/30 backdrop-blur-md w-full top-0 border-b border-neutral-700/50 ">
+        <nav className="flex justify-between items-center max-w-6xl mx-auto py-3  px-3 text-gray-300">
             <Link to='/' className="flex gap-2 uppercase items-center ">
           <img className="w-8 h-8" src={logo} alt="A logo of url scanner" />   Url Scanner
             </Link>
